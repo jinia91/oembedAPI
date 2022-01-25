e67611e3a74db14682c7a412cc9267ca6504ad3d
jinia91 <creee77@gmail.com>
2022-01-26 02:38:27 +0900

    최종ver

--- src/main/java/com/test/backend/embedapi/exception/CustomResponseEntityExceptionHandler.java
+++ src/main/java/com/test/backend/embedapi/exception/CustomResponseEntityExceptionHandler.java
@@ -1,10 +1,8 @@
 package com.test.backend.embedapi.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
